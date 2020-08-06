e1539b47af5acc783a2c7bbead12908e36505918
KacperNlp <kacper.nlp@wp.pl>
2020-08-06 15:14:42 +0200

    Updates

---
+++ precache-manifest.8bf4dad982c0f2d50cca86ec8021bf27.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e6cb70659726f9b68f523f097bb262e",
+    "url": "/react-first-app/index.html"
+  },
+  {
+    "revision": "31d557aa718add9c4888",
+    "url": "/react-first-app/static/css/main.aed84361.chunk.css"
+  },
+  {
+    "revision": "9476c286cbb0b47e49d2",
+    "url": "/react-first-app/static/js/2.20b05b94.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-first-app/static/js/2.20b05b94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d557aa718add9c4888",
+    "url": "/react-first-app/static/js/main.90d5bcd5.chunk.js"
+  },
+  {
+    "revision": "eb712c235875520e0e20",
+    "url": "/react-first-app/static/js/runtime-main.574512a7.js"
+  },
+  {
+    "revision": "ca8754c09d88e3c888188872e5c68167",
+    "url": "/react-first-app/static/media/header1.ca8754c0.jpg"
+  },
+  {
+    "revision": "3e7625e972f3515ab7bfe1f3e37cc4c5",
+    "url": "/react-first-app/static/media/header2.3e7625e9.jpg"
+  },
+  {
+    "revision": "1c8309784eb04f222c218229fba3382f",
+    "url": "/react-first-app/static/media/header3.1c830978.jpg"
+  }
+]);
